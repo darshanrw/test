f8f4c43136520f1cd061e28f8660392ece500502
Darshan <wadekar.darshan04@gmail.com>
2021-09-16 14:32:23 +0530

    add user

--- user.tf
+++ user.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+  tenant_id = "7f2d10e5-77f7-4aec-bb6c-7dadd2b41b90"
+}
+
 resource "azuread_user" "testusers" {
   user_principal_name = "akmrx@gmail.com"
   display_name        = "akmrx"
