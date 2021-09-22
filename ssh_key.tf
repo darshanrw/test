947f54b43dc8880197594c1b1fcfdac016c5640b
Darshan Wadekar <wadekar.darshan04@gmail.com>
2021-09-22 13:31:08 +0530

    Delete ssh_key.tf

--- ssh_key.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_ssh_public_key" "ssh_key" {
-  name                = "SSH_KEY"
-  resource_group_name = azurerm_resource_group.testrg.name
-  location            = var.location
-  public_key          = file("~/.ssh/id_rsa.pub")
-}
